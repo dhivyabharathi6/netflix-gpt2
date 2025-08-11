7da8c1a054415701ce0edaf804979c87c69d1a30
Gokul Prasath <gokulprasath@Gokuls-MacBook-Air.local>
2025-08-11 19:24:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Body from './components/Body'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <Body/>
+    </>
+  )
+}
+
+export default App
